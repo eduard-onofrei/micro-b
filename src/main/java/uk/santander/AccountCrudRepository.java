04d41624562e05421cab086e666b4075f0362961
Eduard Onofrei & Xabier Garcia de Vicuña <eduard.onofrei+xabier.garcia@santander.co.uk>
2021-10-18 11:23:16 +0200

    wip

--- src/main/java/uk/santander/AccountCrudRepository.java
+++ src/main/java/uk/santander/AccountCrudRepository.java
@@ -3,11 +3,12 @@ package uk.santander;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface AccountCrudRepository
         extends ReactiveCrudRepository<Account, String> {
 
-    Flux<Account> findAllByValue(String value);
+    Flux<Account> findAllByValue(Double value);
     Flux<Account> findAllByOwner(String owner);
 }
--- src/main/java/uk/santander/Controller.java
+++ src/main/java/uk/santander/Controller.java
@@ -1,27 +1,49 @@
 package uk.santander;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class Controller {
     private static final int COMPLEXITY = 1000000;
+    private static final String SERVICE1 = "http://localhost:8080";
+
     @Autowired
     private final AccountCrudRepository accountCrudRepository;
 
+    @Autowired
+    private WebClient webClient;
+
     @PostMapping
     public Mono<Account> createAccount(@RequestBody @Valid Account account){
-        return accountCrudRepository.save(account);
+        log.info("Request with {}", account);
+        final Mono<Account> accountMono = accountCrudRepository.save(account);
+
+        return accountMono.map(acc -> {
+            accountCrudRepository.findAllByValue(acc.getValue())
+                    .map(acc1 -> {
+                        process(acc1);
+                        return acc1;
+                    });
+            return acc;
+        });
     }
 
     @GetMapping
@@ -33,5 +55,13 @@ public class Controller {
                     }
                     return account;
                 });
+    }
+
+    private void process(Account account) {
+        webClient.post()
+                .uri(SERVICE1)
+                .bodyValue(account)
+                .retrieve()
+                .bodyToMono(Account.class);
     }
 }
